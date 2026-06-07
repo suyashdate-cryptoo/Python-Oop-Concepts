d82bb7a7d4832b0a621f7fa22fb6a5abdf7d36fc
Suyash Zumbar Date <suyashdate47@gmail.com>
2026-06-07 14:47:29 +0530

    Added classes and objects example

---
+++ 01_classes_objects.py
@@ -0,0 +1,27 @@
+# ==========================================
+# Classes and Objects in Python
+# ==========================================
+
+# A class is a blueprint for creating objects.
+# An object is an instance of a class.
+
+# ------------------------------------------
+# Class Definition
+# ------------------------------------------
+
+class Student:
+
+    # Method
+    def display_info(self):
+        print("Name: Shweta")
+        print("Course: Artificial Intelligence and Data Science")
+
+
+# ------------------------------------------
+# Creating Object
+# ------------------------------------------
+
+student1 = Student()
+
+# Calling Method
+student1.display_info()
